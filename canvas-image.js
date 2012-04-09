6f5534a440b7bf353042f52721b689a356a52a8a
Geoff Stokes <geoff@geoffstokes.net>
2012-04-09 19:40:14 +1000

    Implemented Atkinson Dithering algorithm.

--- canvas-image.js
+++ canvas-image.js
@@ -28,6 +28,9 @@ function draw()
 
 	}
 
+	dither_atkinson(displayImageData);
+
+
 	displayContext.putImageData(displayImageData, 0, 0);
 
 }
@@ -39,8 +42,7 @@ function greyscale_luminance(image)
 	for (var i = 0; i <= image.data.length; i += 4)
 	{
 
-		image.data[i] = image.data[i + 1] = image.data[i + 2] =
-			Math.round(image.data[i] * 0.21 + image.data[i + 1] * 0.71 + image.data[i + 2] * 0.07);
+		image.data[i] = image.data[i + 1] = image.data[i + 2] = parseInt(image.data[i] * 0.21 + image.data[i + 1] * 0.71 + image.data[i + 2] * 0.07, 10);
 
 	}
 
@@ -55,13 +57,52 @@ function greyscale_average(image)
 	for (var i = 0; i <= image.data.length; i += 4)
 	{
 
-		image.data[i] = image.data[i + 1] = image.data[i + 2] =
-			Math.round((image.data[i] + image.data[i + 1] + image.data[i + 2]) / 3);
+		image.data[i] = image.data[i + 1] = image.data[i + 2] = parseInt((image.data[i] + image.data[i + 1] + image.data[i + 2]) / 3, 10);
 
 	}
 
 	return image;
 
+}
+
+// Apply Atkinson Dither to Image Data
+function dither_atkinson(image)
+{
+
+	imageWidth	= image.width;
+	imageLength	= image.data.length
+
+	for (currentPixel = 0; currentPixel <= imageLength; currentPixel += 4)
+	{
+
+		if (image.data[currentPixel] <= 128)
+		{
+
+			newPixelColour = 0;
+
+		}
+		else
+		{
+
+			newPixelColour = 255;
+
+		}
+
+		err = parseInt((image.data[currentPixel] - newPixelColour) / 8, 10);
+		image.data[currentPixel] = newPixelColour;
+
+		image.data[currentPixel + 4]						+= err;
+		image.data[currentPixel + 8]						+= err;
+		image.data[currentPixel + (4 * imageWidth) - 4]		+= err;
+		image.data[currentPixel + (4 * imageWidth)]			+= err;
+		image.data[currentPixel + (4 * imageWidth) + 4]		+= err;
+		image.data[currentPixel + (8 * imageWidth)]			+= err;
+
+		image.data[currentPixel + 1] = image.data[currentPixel + 2] = image.data[currentPixel];
+
+	}
+
+	return image.data;
 }
 
 function setup()
